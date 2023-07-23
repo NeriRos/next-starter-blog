9dc9d9eb465a11a81e38e3895d4b4b0fa43deb84
Neriya Rosner <neri.coder@gmail.com>
2023-07-23 14:40:01 +0300

    design with tailwind

--- app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
+++ app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
@@ -1,26 +1,23 @@
 "use client";
 
 import React from "react";
-
-import Styles from './PostsFeed.module.css';
 import {usePostsFeed} from "@/app/(posts)/(pages)/posts/components/PostsFeed/usePostsFeed";
 import {TEXTS} from "@/app/(posts)/(pages)/posts/components/PostsFeed/texts";
 import {PostsSearch} from "@/app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch";
 import {Post} from "@/app/(posts)/lib/models/Post";
-import clsx from "clsx";
 import {PostFeedItem} from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem";
 
 export const PostsFeed = () => {
     const {posts, isLoading} = usePostsFeed();
 
     return (
-        <div className={Styles.postsContainer}>
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
             {isLoading ? <span>{TEXTS.loadingText}</span> :
                 <PostsSearch posts={posts}>
                     {(filteredPosts: Post[]) =>
                         filteredPosts.length === 0 ?
                             <span>{TEXTS.NOT_ITEMS_FOUND}</span> :
-                            <div className={clsx([Styles.posts, 'grid'])}>
+                            <div className="grid">
                                 {filteredPosts.map((post: Post) => <PostFeedItem
                                     key={post.id}
                                     post={post}/>)}
