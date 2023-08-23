7a36007a81d4b2bbcdb6653c797ecdfe76319982
Neriya Rosner <neri.coder@gmail.com>
2023-08-23 14:54:37 +0300

    remove render props and save posts state in feed

--- app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
+++ app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
@@ -1,29 +1,17 @@
-"use client";
+"use client"
 
-import React from "react";
-import {usePostsFeed} from "@/app/(posts)/(pages)/posts/components/PostsFeed/usePostsFeed";
-import {TEXTS} from "@/app/(posts)/(pages)/posts/components/PostsFeed/texts";
+import React, {useState} from "react";
 import {PostsSearch} from "@/app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch";
 import {Post} from "@/app/(posts)/lib/models/Post";
 import {PostFeedItem} from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem";
 
-export const PostsFeed = () => {
-    const {posts, isLoading} = usePostsFeed();
+export const PostsFeed = (props: { posts: Post[] }) => {
+    const [filteredPosts, setFilteredPosts] = useState<Post[]>(props.posts)
 
-    return (
-        <div className="w-full flex flex-col gap-4 justify-center items-center">
-            {isLoading ? <span>{TEXTS.loadingText}</span> :
-                <PostsSearch posts={posts}>
-                    {(filteredPosts: Post[]) =>
-                        filteredPosts.length === 0 ?
-                            <span>{TEXTS.NOT_ITEMS_FOUND}</span> :
-                            <div className="grid">
-                                {filteredPosts.map((post: Post) => <PostFeedItem
-                                    key={post.id}
-                                    post={post}/>)}
-                            </div>}
-                </PostsSearch>
-            }
-        </div>
-    )
+
+    return <>
+        <PostsSearch posts={filteredPosts} onPostsFilter={(posts: Post[]) => setFilteredPosts(posts)}/>
+        {filteredPosts.map((post: Post) => <PostFeedItem key={post.id} post={post}/>)}
+    </>
+
 }
