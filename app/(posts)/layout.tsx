1d7025059615a5f2fee97da75d1ccdb316af5300
Neriya Rosner <neri.coder@gmail.com>
2023-08-23 15:15:22 +0300

    remove html from inner layout

--- app/(posts)/layout.tsx
+++ app/(posts)/layout.tsx
@@ -1,18 +1,6 @@
-import "@/styles/globals.css"
 import {Metadata} from "next"
-import {Inter} from "next/font/google"
-import {Toaster} from "react-hot-toast"
 import React from "react"
-import {Providers} from "@/app/Providers";
 import {METADATA} from "@/app/(posts)/consts";
-import Styles from "@/components/Layout/Layout.module.css";
-import {Header} from "@/components/Layout";
-
-const inter = Inter({
-    variable: "--font-inter",
-    subsets: ["latin"],
-})
-
 
 export const metadata: Metadata = {
     title: METADATA.title,
@@ -25,27 +13,10 @@ export const metadata: Metadata = {
     metadataBase: new URL(METADATA.base),
     themeColor: METADATA.themeColor,
 }
-export default async function RootLayout({
-                                             children,
-                                         }: {
+export default async function Layout({
+                                         children,
+                                     }: {
     children: React.ReactNode;
 }) {
-
-    return (
-        <html lang="en">
-        <body className={inter.variable}
-              suppressHydrationWarning={true}>
-        <Providers>
-            <Toaster/>
-            <Header />
-            {/*<Suspense fallback="Loading...">*/}
-            {/*    <AuthStatus/>*/}
-            {/*</Suspense>*/}
-            <main className={Styles.main}>
-                {children}
-            </main>
-        </Providers>
-        </body>
-        </html>
-    )
+    return <>{children}</>
 }
