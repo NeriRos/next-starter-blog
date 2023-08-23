3ac95d5eb53e1d816ce6520903f637447367694c
Neriya Rosner <neri.coder@gmail.com>
2023-08-23 14:53:21 +0300

    deanonymize function

--- app/_core/components/Dropdown/Dropdown.tsx
+++ app/_core/components/Dropdown/Dropdown.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+
+import {ChangeEvent} from "react";
+
 export type DropdownProps = {
     options: { value: string, label: string }[],
     onChange: (value: string) => void,
-    value: string
+    value?: string
 }
 
 export const Dropdown = (props: DropdownProps) => {
-    return <select value={props.value} onChange={(e) => props.onChange(e.target.value)}>
+    const onSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        props.onChange(event.target.value);
+    }
+
+    return <select value={props.value} onChange={onSelectChange}>
         {props.options.map((option) => <option value={option.value}>{option.label}</option>)}
     </select>
 }
