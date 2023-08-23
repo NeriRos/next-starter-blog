d46d1c8d99bdbaee17c3c3b0fde9ed31423e8d79
Neriya Rosner <neri.coder@gmail.com>
2023-08-23 15:16:07 +0300

    use exposed function

--- app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
+++ app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
@@ -1,16 +1,16 @@
 "use client"
 
-import React, {useState} from "react";
+import React from "react";
 import {PostsSearch} from "@/app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch";
 import {Post} from "@/app/(posts)/lib/models/Post";
 import {PostFeedItem} from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem";
+import {usePostsFeed} from "@/app/(posts)/(pages)/posts/components/PostsFeed/usePostsFeed";
 
 export const PostsFeed = (props: { posts: Post[] }) => {
-    const [filteredPosts, setFilteredPosts] = useState<Post[]>(props.posts)
-
+    const {filteredPosts, setPosts} = usePostsFeed(props.posts)
 
     return <>
-        <PostsSearch posts={filteredPosts} onPostsFilter={(posts: Post[]) => setFilteredPosts(posts)}/>
+        <PostsSearch posts={filteredPosts} onPostsFilter={setPosts}/>
         {filteredPosts.map((post: Post) => <PostFeedItem key={post.id} post={post}/>)}
     </>
 
