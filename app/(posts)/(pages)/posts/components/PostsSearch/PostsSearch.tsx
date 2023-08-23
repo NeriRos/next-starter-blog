104e153206bbfcdc10b2cd2abc1c200730b74131
Neriya Rosner <neri.coder@gmail.com>
2023-08-23 14:54:44 +0300

    render function

--- app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch.tsx
+++ app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch.tsx
@@ -1,18 +1,21 @@
+"use client";
+
 import Styles from './PostsSearch.module.css';
 import {Post} from "@/app/(posts)/lib/models/Post";
 import {TEXTS} from "@/app/(posts)/(pages)/posts/components/PostsSearch/texts";
 import {usePostsSearch} from "@/app/(posts)/(pages)/posts/components/PostsSearch/usePostsSearch";
+import React, {ReactNode} from "react";
 
 export const PostsSearch = (props: {
     posts: Post[],
-    children: (posts: Post[]) => React.ReactNode | React.ReactNode[],
     onPostsFilter?: (posts: Post[]) => void,
+    render?: (posts: Post[]) => ReactNode | ReactNode[]
 }) => {
     const {filteredPosts, searchPosts, searchText} = usePostsSearch(props.posts, props.onPostsFilter);
 
     return <>
         <input placeholder={TEXTS.SEARCH_PLACEHOLDER} className={Styles.searchInput} type="text" value={searchText}
                onChange={(e) => searchPosts(e.target.value)}/>
-        {props.children(filteredPosts)}
+        {filteredPosts.length > 0 ? props?.render?.(filteredPosts) : <span>{TEXTS.NOT_ITEMS_FOUND}</span>}
     </>
 }
