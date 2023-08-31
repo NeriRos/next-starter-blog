d72715087bcb18e50e95f0a72410f814ff706da4
Neriya Rosner <neri.coder@gmail.com>
2023-08-31 22:52:44 +0300

    get user from server

--- app/(authentication)/context/UserProvider.tsx
+++ app/(authentication)/context/UserProvider.tsx
@@ -1,29 +1,37 @@
 "use client"
 
-import { UserContext } from "@/app/(authentication)/context/UserContext"
 import { useSession } from "next-auth/react"
 import { ReactNode, useEffect, useState } from "react"
-import { LoadingDots } from "@/components/LoadingDots"
 import { IUser } from "@/app/(authentication)/lib/interfaces/IUser"
+import axios from "axios"
+import { LoadingDots } from "@/components/LoadingDots"
 
 export const UserProvider = (props: { children: ReactNode }) => {
     const { data } = useSession()
 
     const [user, setUser] = useState<IUser>()
 
-    // useEffect(() => {
-    //     if (!data?.user) return
-    //
-    //     setUser({})
-    // }, [data?.user])
+    const getUser = async () => {
+        if (!data?.user) return
+
+        const res = await axios.get(`/api/get-user?email=${data.user.email}`)
+        console.log(res)
+        setUser(res.data)
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [])
 
     if (!user) {
         return <LoadingDots />
     }
 
     return (
-        <UserContext.Provider value={{ user }}>
+        <>
+            {/*<UserContext.Provider value={{ user }}>*/}
             {props.children}
-        </UserContext.Provider>
+            {/*</UserContext.Provider>*/}
+        </>
     )
 }
