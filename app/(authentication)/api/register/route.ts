fe31aa1733ac2cb04c97dd7cb9210ab63efb1488
Neriya Rosner <neri.coder@gmail.com>
2023-08-31 22:00:11 +0300

    remove log

--- app/(authentication)/api/register/route.ts
+++ app/(authentication)/api/register/route.ts
@@ -10,14 +10,17 @@ const authenticationService = createAuthenticationService({
 
 export async function POST(req: Request) {
     const registerData: RegisterArgs = await req.json()
-    console.log(registerData)
+
     try {
         const user = await authenticationService.register(registerData)
 
         return NextResponse.json(user)
     } catch (e) {
         if (e instanceof UserAlreadyExists) {
-            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+            return NextResponse.json(
+                { error: "User already exists" },
+                { status: 400 }
+            )
         }
 
         return NextResponse.json({ error: "Unhandled error" }, { status: 500 })
