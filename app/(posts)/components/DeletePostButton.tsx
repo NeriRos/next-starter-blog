92d5383bd76fe0f0ae39c825db8a556792c3bc8e
Neriya Rosner <neri.coder@gmail.com>
2023-08-31 21:32:30 +0300

    format

--- app/(posts)/components/DeletePostButton.tsx
+++ app/(posts)/components/DeletePostButton.tsx
@@ -1,15 +1,19 @@
-import {Button} from "@/components/Button";
-import axios from "axios";
+import { Button } from "@/components/Button"
+import axios from "axios"
 
-export const DeletePostButton = async (props: { postId: number }) => {
-
+export const DeletePostButton = (props: { postId: number }) => {
     const deletePost = async () => {
-        await axios.delete(`/posts/${props.postId}`);
+        await axios.delete(`/posts/${props.postId}`)
     }
 
-    return <div className="flex flex-col space-y-2">
-        <Button onClick={deletePost} type="ghost" className={"text-3xl text-black"}>
-            Delete
-        </Button>
-    </div>
+    return (
+        <div className="flex flex-col space-y-2">
+            <Button
+                onClick={deletePost}
+                type="ghost"
+                className={"text-3xl text-black"}>
+                Delete
+            </Button>
+        </div>
+    )
 }
