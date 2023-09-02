1f89a8cd305f21a0abeeba8ee917d33fe1645289
Neriya Rosner <neriyarosner@gmail.com>
2023-09-02 12:50:10 +0300

    add comment model

---
+++ app/(posts)/modules/Comments/lib/models/Comment.ts
@@ -0,0 +1,26 @@
+import "server-only";
+import {IComment} from "@/app/(posts)/modules/Comments/lib/interfaces/IComment";
+
+export class Comment implements IComment {
+    constructor(
+        public content: string,
+        public postId: number,
+        public authorId: number,
+        public id: number
+    ) {
+    }
+
+    public static fromJson(json: any): Comment {
+        const {content, postId, authorId, id} = json;
+        return new Comment(content, postId, authorId, id);
+    }
+
+    public toJson(): any {
+        return {
+            content: this.content,
+            postId: this.postId,
+            authorId: this.authorId,
+            id: this.id
+        }
+    }
+}
