da91854cb25383998f317f8c7c60222cc732da3d
Neriya Rosner <neriyarosner@gmail.com>
2023-09-02 14:04:11 +0300

    format

--- app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
+++ app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 import React from "react"
-import { PostsSearch } from "@/app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch"
-import { Post } from "@/app/(posts)/lib/models/Post"
-import { PostFeedItem } from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem"
-import { usePostsFeed } from "@/app/(posts)/(pages)/posts/components/PostsFeed/usePostsFeed"
+import {PostsSearch} from "@/app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch"
+import {Post} from "@/app/(posts)/lib/models/Post"
+import {PostFeedItem} from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem"
+import {usePostsFeed} from "@/app/(posts)/(pages)/posts/components/PostsFeed/usePostsFeed"
 
 export const PostsFeed = () => {
-    const { posts, filteredPosts, updatePosts } = usePostsFeed()
+    const {posts, filteredPosts, updatePosts} = usePostsFeed()
 
     return (
         <div className="w-full flex flex-col gap-4 justify-center items-center">
