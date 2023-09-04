f62bb7fbd58eceb2f03f7f678e28f6450754e3e9
Neriya Rosner <neri.coder@gmail.com>
2023-09-04 18:27:58 +0300

    remove search to use server component

--- app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
+++ app/(posts)/(pages)/posts/components/PostsFeed/PostsFeed.tsx
@@ -1,21 +1,16 @@
-"use client"
-
 import React from "react"
-import {PostsSearch} from "@/app/(posts)/(pages)/posts/components/PostsSearch/PostsSearch"
-import {Post} from "@/app/(posts)/lib/models/Post"
-import {PostFeedItem} from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem"
-import {usePostsFeed} from "@/app/(posts)/(pages)/posts/components/PostsFeed/usePostsFeed"
+import { Post } from "@/app/(posts)/lib/models/Post"
+import { PostFeedItem } from "@/app/(posts)/(pages)/posts/components/PostsFeed/PostFeedItem"
+import { postsService } from "@/app/(posts)/lib/services/PostsService"
 
-export const PostsFeed = () => {
-    const {posts, filteredPosts, updatePosts} = usePostsFeed()
+export const PostsFeed = async () => {
+    // const { posts, filteredPosts, updatePosts } = usePostsFeed()
+
+    const posts = await postsService.getAllPosts()
 
     return (
         <div className="w-full flex flex-col gap-4 justify-center items-center">
-            <PostsSearch
-                posts={posts}
-                onPostsFilter={updatePosts}
-            />
-            {filteredPosts.map((post: Post) => (
+            {posts.map((post: Post) => (
                 <PostFeedItem
                     key={post.id}
                     post={post}
