503ffbea4f6150e332f667efb4ede2fe6cf37470
Neriya Rosner <neri.coder@gmail.com>
2023-09-04 18:28:42 +0300

    use form to delete

--- app/(posts)/components/DeletePostButton.tsx
+++ app/(posts)/components/DeletePostButton.tsx
@@ -3,17 +3,19 @@ import axios from "axios"
 
 export const DeletePostButton = (props: { postId: number }) => {
     const deletePost = async () => {
+        "use server"
         await axios.delete(`/posts/${props.postId}`)
     }
 
     return (
         <div className="flex flex-col space-y-2">
-            <Button
-                onClick={deletePost}
-                type="ghost"
-                className={"text-3xl text-black"}>
-                Delete
-            </Button>
+            <form action={deletePost}>
+                <Button
+                    type="ghost"
+                    className={"text-3xl text-black"}>
+                    Delete
+                </Button>
+            </form>
         </div>
     )
 }
