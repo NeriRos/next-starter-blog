848eb34d498afe233887793ca78e33eecd666ede
Neriya Rosner <neri.coder@gmail.com>
2023-09-04 18:27:24 +0300

    add delete post

--- app/(posts)/(modules)/comments/components/Comment.tsx
+++ app/(posts)/(modules)/comments/components/Comment.tsx
@@ -1,5 +1,7 @@
-import {IComment} from "@/app/(posts)/(modules)/comments/lib/interfaces/IComment";
-import {usersService} from "@/app/(authentication)/lib/services/UsersService";
+import { IComment } from "@/app/(posts)/(modules)/comments/lib/interfaces/IComment"
+import { usersService } from "@/app/(authentication)/lib/services/UsersService"
+import { commentsService } from "@/app/(posts)/(modules)/comments/lib/services/CommentsService"
+import { BiExit } from "react-icons/bi"
 
 type CommentProps = {
     comment: IComment
@@ -7,19 +9,28 @@ type CommentProps = {
 
 export const Comment = async (props: CommentProps) => {
     console.log(props.comment)
-    const author = await usersService.getUserById(props.comment.userId);
+    const author = await usersService.getUserById(props.comment.userId)
 
-    if (!author) return null;
+    const deletePost = async () => {
+        "use server"
+        if (!props.comment.id) return null
+        await commentsService.deleteComment(props.comment.id)
+    }
+
+    if (!author) return null
 
     return (
-        <div className="bg-white p-4 rounded shadow-md w-3/4 mx-auto">
+        <div className="bg-white p-4 rounded shadow-md w-3/4 mx-auto relative">
+            <form action={deletePost}>
+                <button className="border-none bg-none">
+                    <BiExit className="absolute top-1 right-1" />
+                </button>
+            </form>
             <div className="flex items-center mb-2">
                 <div className="bg-gray-300 rounded-full w-8 h-8 mr-2"></div>
                 <div className="text-xl font-bold">{author?.name}</div>
             </div>
-            <div className="text-gray-700">
-                {props.comment.content}
-            </div>
+            <div className="text-gray-700">{props.comment.content}</div>
         </div>
     )
 }
