245f4a03b780e0a435df50361b5fd568fd73e3f5
Neriya Rosner <neri.coder@gmail.com>
2023-09-06 20:39:20 +0300

    edit category

--- app/(posts)/components/PostEditForm.tsx
+++ app/(posts)/components/PostEditForm.tsx
@@ -4,8 +4,13 @@ import { TextArea } from "@/components/Form/TextArea"
 import { IPost } from "@/app/(posts)/lib/interfaces/IPost"
 import { postsService } from "@/app/(posts)/lib/services/PostsService"
 import { revalidatePath } from "next/cache"
+import { ICategory } from "@/app/(posts)/(modules)/categories/lib/interfaces/ICategory"
+import { categoriesService } from "@/app/(posts)/(modules)/categories/lib/services/CategoriesService"
+import { Select } from "@/components/Select/Select"
 
-export const PostEditForm = (props: { post: IPost }) => {
+export const PostEditForm = async (props: { post: IPost }) => {
+    const categories = await categoriesService.getAllCategories()
+
     const editPost = async (formData: FormData) => {
         "use server"
         if (!props.post.id) return null
@@ -13,6 +18,7 @@ export const PostEditForm = (props: { post: IPost }) => {
         await postsService.updatePost(props.post.id, {
             title: formData.get("title") as string,
             content: formData.get("content") as string,
+            categoryId: Number(formData.get("category") as string),
         })
 
         revalidatePath("/posts")
@@ -31,6 +37,14 @@ export const PostEditForm = (props: { post: IPost }) => {
                     name={"content"}
                     rows={5}
                 />
+                <Select
+                    name={"category"}
+                    options={categories.map((category: ICategory) => ({
+                        label: category.name,
+                        value: category.id,
+                    }))}
+                />
+                <hr />
                 <Button
                     type="ghost"
                     className={"text-3xl text-black"}>
