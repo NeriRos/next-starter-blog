256cf38d176500ec1e9a67ef7dc846e7c15cf554
Neriya Rosner <neri.coder@gmail.com>
2023-09-06 21:02:47 +0300

    create or edit

--- app/(posts)/components/PostCreateOrEditForm.tsx
+++ app/(posts)/components/PostCreateOrEditForm.tsx
@@ -7,33 +7,66 @@ import { revalidatePath } from "next/cache"
 import { ICategory } from "@/app/(posts)/(modules)/categories/lib/interfaces/ICategory"
 import { categoriesService } from "@/app/(posts)/(modules)/categories/lib/services/CategoriesService"
 import { Select } from "@/components/Form/Select"
+import { getServerSession } from "next-auth"
+import { usersService } from "@/app/(authentication)/lib/services/UsersService"
+import { Post } from "@/app/(posts)/lib/models/Post"
 
-export const PostEditForm = async (props: { post: IPost }) => {
+export const PostCreateOrEditForm = async (props: { post?: IPost }) => {
     const categories = await categoriesService.getAllCategories()
 
-    const editPost = async (formData: FormData) => {
+    const createPost = async (postData: Omit<IPost, "authorId" | "id">) => {
         "use server"
-        if (!props.post.id) return null
+        const session = await getServerSession()
+        const user = await usersService.getUserByEmail(
+            session?.user?.email || ""
+        )
+        if (!user) return console.log("NO USER")
 
-        await postsService.updatePost(props.post.id, {
+        await postsService.createPost(
+            Post.fromJson({
+                ...postData,
+                authorId: Number(user.id),
+            })
+        )
+    }
+
+    const editPost = async (postData: Omit<IPost, "authorId">) => {
+        "use server"
+        await postsService.updatePost(postData.id, postData)
+    }
+
+    const confirm = async (formData: FormData) => {
+        "use server"
+        const postData: Omit<IPost, "authorId" | "id"> = {
             title: formData.get("title") as string,
             content: formData.get("content") as string,
             categoryId: Number(formData.get("category") as string),
-        })
+        }
+
+        if (props.post?.id) {
+            await editPost({
+                ...postData,
+                id: props.post?.id,
+            })
+        } else {
+            await createPost(postData)
+        }
 
         revalidatePath("/posts")
     }
 
     return (
-        <form action={editPost}>
+        <form action={confirm}>
             <div className="flex flex-col space-y-2 p-3">
                 <Input
-                    defaultValue={props.post.title}
+                    placeholder={"Title"}
+                    defaultValue={props.post?.title}
                     type="text"
                     name={"title"}
                 />
                 <TextArea
-                    defaultValue={props.post.content}
+                    placeholder={"Content"}
+                    defaultValue={props.post?.content}
                     name={"content"}
                     rows={5}
                 />
@@ -48,7 +81,7 @@ export const PostEditForm = async (props: { post: IPost }) => {
                 <Button
                     type="ghost"
                     className={"text-3xl text-black"}>
-                    Save
+                    {props.post?.id ? "Edit" : "Create"}
                 </Button>
             </div>
         </form>
