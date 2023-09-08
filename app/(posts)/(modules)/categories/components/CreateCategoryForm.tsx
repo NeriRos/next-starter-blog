36c3f292dcdea5e1ecab6f3592d26293f652d0b0
Neriya Rosner <neri.coder@gmail.com>
2023-09-08 12:32:52 +0300

    display category items

--- app/(posts)/(modules)/categories/components/CreateCategoryForm.tsx
+++ app/(posts)/(modules)/categories/components/CreateCategoryForm.tsx
@@ -4,12 +4,15 @@ import { revalidatePath } from "next/cache"
 import clsx from "clsx"
 import { Input } from "@/components/Form/Input"
 import { categoriesService } from "@/app/(posts)/(modules)/categories/lib/services/CategoriesService"
+import { CategoryItem } from "@/app/(posts)/(modules)/categories/components/CategoryItem"
 
 type CreateCategoryButtonProps = {
     className?: string
 }
 
 export const CreateCategoryForm = async (props: CreateCategoryButtonProps) => {
+    const categories = await categoriesService.getAllCategories()
+
     async function createCategory(formData: FormData) {
         "use server"
         const name = formData.get("name") as string
@@ -43,6 +46,14 @@ export const CreateCategoryForm = async (props: CreateCategoryButtonProps) => {
                 className={"text-3xl text-black"}>
                 Create Category
             </Button>
+            <div className={"flex gap-2"}>
+                {categories.map((category, index) => (
+                    <CategoryItem
+                        key={category.id}
+                        category={category}
+                    />
+                ))}
+            </div>
         </form>
     )
 }
